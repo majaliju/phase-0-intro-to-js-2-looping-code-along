d5773d9a821df8f1816d6f10533109e43a34cf25
majaliju <majalijunyc@gmail.com>
2022-01-24 16:08:42 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const cardsList = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cardsList, event = "surprise"){
+    const messagesForCards = [];
+    for (let i=0; i < cardsList.length; i++){
+        messagesForCards[i] = `Thank you, ${cardsList[i]}, for the wonderful ${event} gift!`;
+    }
+    return messagesForCards;
+}
+
+function countDown(int){
+    while (int >= 0){
+        console.log(int);
+        int--;
+    }
+}
